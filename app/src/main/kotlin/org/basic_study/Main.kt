28e170d0b54f4f404dc056980d3f2609dde1a4de
vincenciusadyatma <adyatmav@gmail.com>
2025-09-09 15:30:23 +0700

    feat: change default kotlin split function into my handmade function

--- app/src/main/kotlin/org/basic_study/Main.kt
+++ app/src/main/kotlin/org/basic_study/Main.kt
@@ -1,12 +1,41 @@
+/*nama atau namespace tempat file ini berada, file ini berada di dalam package basic_study yang terletak 
+di dalam package org*/
 package org.basic_study
 
+/*mendefinisikan variabel bernama teks yang bertipe data String yang diisi dengan nilai string
+"ini kelas pemrograman mobile" */
 var teks: String = "ini kelas pemrograman mobile"
 
-fun stringToElemen(teks: String): List<String>{
-    var teksList = teks.split(" ")
-    return teksList
+
+/*mendefinisikan sebuah fungsi bernama stringToElemen yang mempunyai satu parameter bertipe data string bernama
+teks kemudian memiliki tipe nilai yang akan dikembalikan yaitu bertipe data sebuah List yang bersifat mutable atau dapat berubah yang memiliki tipe
+generic bertipe data String . */
+fun stringToElemen(teks: String, delimiter: Char): MutableList<String>{
+    //membuat sebuah list kosong baru yang memiliki tipe generic String yang disimpan dalam variabel listElement
+   var listElement = mutableListOf<String>()
+   //membuat sebuah variabel string kosong bernama kata
+   var kata = ""
+   //melakukan perulangan pada setiap karakter pada sebuah teks bertipe string
+   for (karakter in teks){
+        //jika karakter tersebut merupakan ppemisah seperti spasi atau karakter lainya, maka tambahkan isi varaibel kata kedalam sebuah list
+        //kemudian isi variabel kata dikosongkan kembali
+        if(karakter == delimiter){
+            listElement.add(kata)
+            kata = ""
+        //jika karakter tersebut bukan delimiter atau pemisah, maka tambahkan karakter tersebut ke sebuah string pada variabel kata
+        }else{
+            kata += karakter
+        }
+   }
+   //jika perulangan sudah selesai, maka kembalikan nilai list yang sudah berisi elemen elemen kata string
+   return listElement
 }
 
+
 fun main(){
-    print(stringToElemen(teks))
+   for (kata in stringToElemen(teks,' ')){
+        println(kata)
+   }
+   
+    
 }
